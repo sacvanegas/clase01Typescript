9afbe30f875b5b0f9ac70545933f1fa91514b66c
andres <you@example.com>
2018-08-16 06:19:49 -0500

    Añadido constructor y llamado a objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { Persona, concatenar } from './Persona';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
   title = 'app';
+
+
+  constructor() {
+    let persona = new Persona("andres", "masculino");
+    //persona.nombre="camilo";
+    //persona.edad=25;
+    console.log(concatenar(persona.nombre, persona.sexo));
+    
+  }
+
 }
